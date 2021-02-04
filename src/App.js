d7aa4fc603ed450b38403f20e5e17d53b6aba8c6
William Porr <porrliam@gmail.com>
2021-02-04 16:31:51 -0600

    Added email sign up frontend and colorful button

--- src/App.js
+++ src/App.js
@@ -2,18 +2,55 @@ import React from 'react';
 import marked from 'marked';
 import Path from 'path';
 import leaves from './leaves.png';
+import drip from './drip.webp';
+import holo from './holo.png';
 //import line1 from './line1.png';
 import line2 from './line2.png';
 import { Link } from 'react-router-dom';
 import './main.css';
 
 const Header = () => (
-  <div class="nav">
-  <Link to='/' style={{float: 'left', margin:'20px'}}>
+  <div id="nav">
+  <Link to='/' style={{float: 'left', }}>
     <img src={line2} height="75px" alt="logo"/>
   </Link>
+  <EmailForm/>
+  <hr class="slant"/>
   </div>
 );
+
+class EmailForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+    };
+
+    this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    //stuff
+    console.log("submitted");
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div class="vertical">
+          <p style={{fontSize: 'small', textAlign: 'center'}}><i>Can I sing to you?</i></p>
+          <div class="horizontal">
+            <input id="name" placeholder="Liam Porr"/>
+            <input type="email" placeholder="liam@sadb.oys" required/>
+            <button type="submit" className="btn movable btn-special">Yes.</button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+}
 
 const Home = () => {
 
