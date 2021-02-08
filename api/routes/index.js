a29f846570fa3eb59615fb22762a20a58124d5e3
William Porr <porrliam@gmail.com>
2021-02-08 12:56:55 -0600

    Email subscriptions now get stored in a mongodb database

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const { MongoClient } = require("mongodb");
 
-/* GET home page. */
-router.post('/subscribe', function(req, res, next) {
-  console.log(req);
-  console.log("HAI");
-  res.send('respond with a resource');
+/* Connect to mongodb client */
+database = "websiteData"
+connectionString = `mongodb+srv://wporr:${process.env.ATLAS_PW}@mailinglist.wsoci.mongodb.net/${database}?retryWrites=true&w=majority`
+MongoClient.connect(connectionString,
+  { useUnifiedTopology: true })
+  .then(client => {
+  console.log("connected to database");
+  const db = client.db(database);
+  const emailCollection = db.collection('emails');
+
+  /* Request handler for subscriptions */
+  router.post('/subscribe', function(req, res, next) {
+    emailCollection.insertOne(req.body)
+      .then(result => {
+        console.log(result);
+      }).catch(error => console.error(error));
+
+  });
 });
+
+
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
