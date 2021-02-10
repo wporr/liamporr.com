a254bdabfa992c531218c3df9a9088de808b18e9
William Porr <porrliam@gmail.com>
2021-02-10 16:48:18 -0600

    Send email confirmations for subscribe/unsubscribe

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const nodemailer = require("nodemailer");
 const { MongoClient } = require("mongodb");
+
+const emailAddress = 'liam@liamporr.com';
+
+/* Connect to email client */
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: emailAddress,
+    pass: process.env.EMAIL_PW
+  }
+});
 
 /* Connect to mongodb client */
 database = "websiteData"
@@ -14,9 +26,37 @@ MongoClient.connect(connectionString,
 
   /* Request handler for subscriptions */
   router.post('/subscribe', function(req, res, next) {
+    if (!("email" in req.body)) {
+      console.error("Subscription calll with no email!");
+      return;
+    }
+
     emailCollection.insertOne(req.body)
       .then(result => {
-        console.log(result);
+
+        /* Send email confirmation */
+        var body;
+
+        if ("name" in req.body) {
+          body = req.body.name;
+        } else {
+          body = "Anonymous";
+        }
+        body += " subscribed with email: " + req.body.email;
+        console.log(body);
+
+        var mailOptions = {
+          from: emailAddress,
+          to: emailAddress,
+          subject: "New subscription!",
+          text: body
+        }
+
+        transporter.sendMail(mailOptions, (err, info) => {
+          if (err) {
+            console.error(err);
+          }
+        });
       }).catch(error => console.error(error));
   });
 
@@ -24,20 +64,31 @@ MongoClient.connect(connectionString,
   router.get('/unsubscribe', function(req, res, next) {
     emailCollection.deleteOne(
       { email: req.query.email }
-    )
-      .then(result => {
-        console.log("user " + req.query.email + " unsubscribed");
-      }).catch(error => console.error(error));
+    ).then(result => {
+        const body = "Email " + req.query.email + " unsubscribed"
+        console.log(body);
 
+        /* Send email confirmation */
+        var mailOptions = {
+          from: emailAddress,
+          to: emailAddress,
+          subject: "User Unsubscribed",
+          text: body
+        }
+
+        transporter.sendMail(mailOptions, (err, info) => {
+          if (err) {
+            console.error(err);
+          }
+        });
+      }).catch(error => console.error(error));
     res.send("unsubscribed");
   });
 });
 
 
-
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
 
 module.exports = router;
