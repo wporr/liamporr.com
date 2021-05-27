cfc540d4997bc96b63edfdf787875ff8bfe80a48
William Porr <porrliam@gmail.com>
2021-05-27 00:05:46 -0500

    Added True Freedom part 1

--- client/src/content.js
+++ client/src/content.js
@@ -1,5 +1,6 @@
 const pages = {
   about: "content/about.md",
+  tf1: "content/tf1.md",
   journaling: "content/journaling.md",
   dontWorkHard: "content/dontWorkHard.md",
   universityAssemblyLine: "content/universityAssemblyLine.md",
